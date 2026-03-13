fix: correct artificial missing mask computation (missing entries were marked as true instead of false)

GenerativeProteomics/models/GainPro/trainer.py
@@ -223,9 +223,10 @@ class Trainer:
         experiment_writer: ExperimentWriter,
     ) -> None:
         
-        print("reference", data.reference)
-        print("missing", data.missing)
-        print("observed mask", data.observed_mask)
+        # print("reference", data.reference)
+        # print("missing", data.missing)
+        # print("observed mask", data.observed_mask)
+        # print("artificial missing mask", data.artificial_missing_mask)
 
         # scaler = ProteomicsScaler()
         
@@ -293,8 +294,17 @@ class Trainer:
         observed_mask_np = observed_mask.detach().cpu().numpy()
         x_true_log2p1_inverse = np.where(observed_mask_np == 0, np.nan, np.power(2, x_true) - 1)
 
+        # print("\n\n\n\n =================== ")
+        # print("true values", x_true_log2p1_inverse)
+        # print("predicted values", x_hat_log2p1_inverse)
+        # print("observed mask", observed_mask)
+        # print("artificial missing mask", artificial_missing_mask)
+        # print("data missing", data.missing)
+        # print("observed mask & artificial missing mask",  torch.logical_and(observed_mask, artificial_missing_mask))
+        # print("\n\n\n\n =================== ")
+
         experiment_writer.result_writer.save_predictions(
-            sample_ids=np.arange(start=0, stop=data.reference.shape[0]),
+            sample_ids=data.sample_names,
             feature_names=data.feature_names,
             true_values=x_true_log2p1_inverse,
             pred_values=x_hat_log2p1_inverse,

GenerativeProteomics/utils/data/dataset_builder.py
@@ -3,6 +3,7 @@ import errno
 import numpy as np
 import pandas as pd
 from pathlib import Path
+from sklearn.preprocessing import StandardScaler
 
 from utils.data.dataset import Data
 from utils.data.helper import (
@@ -41,9 +42,13 @@ class DatasetBuilder:
             self.cell_line = cat.cat.codes
             self.condition_mapping = dict(enumerate(cat.cat.categories))
             self.df = self.df.drop(columns=["condition"])
+        
+        print(f"Original dataset is {self.df.shape}")
 
         self._clean()
         self._log_transform()
+
+        print(f"After preprocessing {self.df.shape}")
 
         self.reference = None
         self.missing = None
@@ -55,6 +60,7 @@ class DatasetBuilder:
 
     def _load(self) -> pd.DataFrame:
         if self.dataset_path.suffix == ".csv":
+            print("Aqui")
             df = load_csv(self.dataset_path)
         elif self.dataset_path.suffix == ".h5ad":
             df = load_anndata(self.dataset_path)
@@ -74,7 +80,6 @@ class DatasetBuilder:
             self.df = drop_proteins_with_missingness_threshold(self.df, self.cfg["dataset"]["max_protein_missingness"])
 
     def _log_transform(self) -> None:
-        # print("NOT Log2-transformed DataFrame:\n", self.df)
         values = self.df.values.astype(float)
         values_transformed = np.log2(values + 1)
 
@@ -83,8 +88,6 @@ class DatasetBuilder:
             index=self.df.index,
             columns=self.df.columns
         )
-        
-        # print("Log2-transformed DataFrame:\n", self.df)
 
     def get_dataset_dir(self) -> Path:
         return self.dataset_path.parent
@@ -96,14 +99,28 @@ class DatasetBuilder:
     ) -> Data:
 
         self.observed_mask = compute_observed_mask(self.df)
+        print("Observed mask", self.observed_mask)
 
         # - Normalize proteins (columns) between [0,1] -
         # observed mask due to the nans
         min_norm = self.df[self.observed_mask].min(axis=0) # min protein value
         max_norm = self.df[self.observed_mask].max(axis=0) # max protein value
-        X_norm = (self.df - min_norm) / (max_norm - min_norm)
-        self.reference = X_norm
+        X_norm = (self.df[self.observed_mask] - min_norm) / (max_norm - min_norm)
 
+        # todo test with standard scaler
+        # scalers = {}
+        # x_scaled = pd.DataFrame(index=self.df.index, columns=self.df.columns, dtype=float)
+        # for col in self.df.columns:
+        #     observed = self.df[col][self.observed_mask[col] == 1].values.reshape(-1, 1)
+        #     scaler = StandardScaler()
+        #     scaler.fit(observed)
+        #     scalers[col] = scaler
+        #     # transform all values
+        #     x_scaled[col] = scaler.transform(self.df[col])
+
+        # self.reference = x_scaled
+
+        self.reference = X_norm
         self.missing = induce_missing(df=self.df, seed=seed, miss_rate=self.cfg["dataset"]["miss_rate"])
 
         print("Dataset shape", self.reference.shape)
@@ -112,13 +129,16 @@ class DatasetBuilder:
             return None
 
         # save missingness metadata
-        self.original_missingness = compute_missing_rate(self.reference)
+        self.original_missingness = compute_missing_rate(self.df)
         self.current_missingness = compute_missing_rate(self.missing)
 
         print(f"Original missing rate: {self.original_missingness:.2%}")
         print(f"Current missing rate: {self.current_missingness:.2%}")
 
-        self.artificial_missing_mask = compute_evaluation_mask(self.reference, self.missing)
+        self.artificial_missing_mask = compute_evaluation_mask(
+            reference_df=self.df,
+            missing_df=self.missing
+        )
 
         if fill_zeros is True:
             self.reference = self.reference.fillna(0)
@@ -134,5 +154,6 @@ class DatasetBuilder:
             cell_line=self.cell_line,
             min_norm=min_norm,
             max_norm=max_norm,
+            # col_scalers=scalers,
         )
         return data

GenerativeProteomics/utils/data/helper.py
@@ -157,49 +157,26 @@ def induce_missing(
     seed: int,
     # ceiling_miss_rate: float=1.0,
     miss_rate: float=0.0,
-) -> pd.DataFrame:
+) -> tuple[pd.DataFrame, pd.DataFrame]:
+
     rng = np.random.default_rng(seed)
 
     df_missing = df.copy()
 
-    current_missingness = compute_missing_rate(df_missing)
-    target_missingness = min(current_missingness + miss_rate, 1.0)
-    if target_missingness == 1.0:
-        logger.warning("Not feasible having a dataset with all missing entries. No additional missingness was induced.")
-        return None
-    # if target_missingness > ceiling_miss_rate:
-    #     logger.warning(f"Target missingness ({target_missingness}) has reached the ceiling ({ceiling_miss_rate}). No additional missingness will be induced.")
-    #     return None
+    current_missing = df_missing.isna().sum().sum()
+    total_entries = df_missing.size
 
-    size = df_missing.size
-    n_target_missing_entries = int(target_missingness * size) # number of missing entries with target missingness rate
+    additional_to_mask = int(miss_rate * total_entries)
 
-    # how many more values to mask
-    n_current_missing_entries = df_missing.isna().sum().sum()
-    additional_to_mask = n_target_missing_entries - n_current_missing_entries
+    # positions that currently have values
+    eligible_positions = np.argwhere(df_missing.notna().values)
 
-    # identify all non-missing (eligible) positions
-    # eligible_positions = np.argwhere(~df_missing.isna().values)
-    
-    # Count current non-missing per column
-    non_missing_per_col = df_missing.notna().sum().values  # shape (n_cols,)
-
-    eligible_positions = []
-
-    for i, j in np.argwhere(~df_missing.isna().values):
-        # Only allow masking if column has more than 1 non-missing entry
-        if non_missing_per_col[j] > 10:
-            eligible_positions.append((i, j))
-
-    eligible_positions = np.array(eligible_positions)
     if additional_to_mask > len(eligible_positions):
-        raise ValueError(f"Cannot induce missingness requested.")
+        raise ValueError("Not enough observed entries to mask.")
 
-    # randomly sample positions to mask
-    to_mask_idx = rng.choice(len(eligible_positions), size=additional_to_mask, replace=False)
-    to_mask = eligible_positions[to_mask_idx]
-
-    for i, j in to_mask:
+    chosen_idx = rng.choice(len(eligible_positions), size=additional_to_mask, replace=False)
+    
+    for i, j in eligible_positions[chosen_idx]:
         df_missing.iat[i, j] = np.nan
 
     return df_missing
@@ -210,7 +187,7 @@ def compute_observed_mask(
     """
     Create binary mask indicating originally observed entries (observed entries = 1, missing = 0).
     """
-    return ~reference_df.isna()
+    return reference_df.notna()
 
 def compute_evaluation_mask(
     reference_df: pd.DataFrame,
@@ -221,7 +198,7 @@ def compute_evaluation_mask(
 
     Used to assess the imputation performance.
     """
-    artificially_removed = reference_df.notna() & missing_df.isna()
+    artificially_removed = ~(~reference_df.isna() & missing_df.isna())
     return artificially_removed
 
 def build_domain(