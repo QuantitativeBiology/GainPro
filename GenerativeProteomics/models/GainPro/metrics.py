17690a4dbed20e957b3699cd9e17ed1d1a84b96a
Rita Gama <rita.gama@tecnico.ulisboa.pt>
2026-04-09 18:00:38 +0100

    fix: reset metrics tracker at the start of each fold

--- GenerativeProteomics/models/GainPro/metrics.py
+++ GenerativeProteomics/models/GainPro/metrics.py
@@ -10,35 +10,16 @@ class Metrics:
         self.train_metrics = self.init_epoch()
         self.val_metrics = self.init_epoch()
 
-        # fix: os nomes das losses deviam ser d_loss and not loss_d
-        # self.discriminator_loss = np.zeros(num_epochs)
-        # self.loss_D_evaluate = np.zeros(num_epochs)
-
-        # self.generator_loss = np.zeros(num_epochs)
-        # self.loss_G_evaluate = np.zeros(num_epochs)
-
-        # self.loss_MSE_train = np.zeros(num_epochs)
-        # self.loss_MSE_train_evaluate = np.zeros(num_epochs) #todo what does mse_train_evaluate mean?
-        # self.loss_RMSE_train = np.zeros(num_epochs)
-
-        # self.loss_MSE_test = np.zeros(num_epochs)
-        # self.loss_RMSE_test = np.zeros(num_epochs)
-
-        # self.cpu = np.zeros(num_epochs)
-        # self.cpu_evaluate = np.zeros(num_epochs)
-
-        # self.ram = np.zeros(num_epochs)
-        # self.ram_evaluate = np.zeros(num_epochs)
-
-        # self.ram_percentage = np.zeros(num_epochs)
-        # self.ram_percentage_evaluate = np.zeros(num_epochs)
-
     def init_epoch(self) -> dict[str, list]:
         return {
             "generator_loss": [],
             "discriminator_loss": [],
             "rmse": [],
         }
+    
+    def init_fold(self) -> None:
+        self.train_metrics = self.init_epoch()
+        self.val_metrics = self.init_epoch()
 
     def get_train_metrics(self) -> dict[str, list]:
         return self.train_metrics
