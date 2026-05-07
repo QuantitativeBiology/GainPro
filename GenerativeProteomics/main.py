d2c3dd77814e62ee25614521815567a1b4b9e2f3
Rita Gama <rita.gama@tecnico.ulisboa.pt>
2026-05-07 16:27:24 +0100

    chore: deprecate train command until future implementation

--- GenerativeProteomics/main.py
+++ GenerativeProteomics/main.py
@@ -1,12 +1,11 @@
 import argparse
 
-from commands import train, benchmark
+from commands import benchmark
 
 def main() -> None:
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(dest="command", required=True)
 
-    train.add_parser(subparsers)
     benchmark.add_parser(subparsers)
 
     args = parser.parse_args()
