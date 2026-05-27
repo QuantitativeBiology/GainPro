d325346b94dea0e135691845d332cdd96800e898
Rita Gama <rita.gama@tecnico.ulisboa.pt>
2026-05-27 16:41:05 +0100

    refactor: adapt `MissForestRImputer` to imputation manager and Pydantic config validation
    - Adapt imputer interface to match imputation manager expected by benchmark pipeline
    - Validate MissForestConfig via Pydantic

--- GenerativeProteomics/wrappers/missforest.py
+++ GenerativeProteomics/wrappers/missforest.py
@@ -1,18 +1,22 @@
 import torch
 import shutil
-import numpy as np
+import logging
+from typing import Optional
 
 from missingpy import MissForest
 
+from utils.helper import load_yaml
 from utils.data.dataset import Data
 from utils.writers.experiment_writer import ExperimentWriter
-from utils.model_hypers import MissForestHypers
+from utils.configs.model_config import MissForestConfig
+
+logger = logging.getLogger(__name__)
 
 class MissForestRImputer():
     def __init__(
         self,
-        missforest_hypers: MissForestHypers,
-    ) -> "MissForestRImputer":
+        cfg: MissForestConfig,
+    ) -> None:
         if shutil.which("R") is None:
             raise RuntimeError(
                 "R executable not found. You must install R (https://cran.r-project.org/) "
@@ -20,30 +24,46 @@ class MissForestRImputer():
             )
 
         try:
-            self.n_tree = missforest_hypers.n_tree
-            self.max_iter = missforest_hypers.max_iter
-            print("Number of trees:", self.n_tree)
-            print("Maximum iterations:", self.max_iter)
+            self.n_tree = cfg.n_tree
+            self.max_iter = cfg.max_iter
+            logger.info(
+                f"\n Number of trees: {self.n_tree}"
+                f"\n Maximum of iterations: {self.max_iter}"
+            )
 
             self.missforest = MissForest(
                 n_estimators=self.n_tree,
                 max_iter=self.max_iter,
                 max_features=None,
-                criterion=("squared_error")  
+                criterion="squared_error" 
             )
-        except Exception as e:  
-            print("The 'missForest' R package is not installed. Please install it in your R environment.")
-            raise e
+        except Exception as e:
+            logger.exception("The 'missForest' R package is not installed. Please install it in your R environment.")
+            raise
+
+    @classmethod
+    def from_config(
+        cls,
+        cfg: MissForestConfig,
+        data: Data,
+    ):
+        return cls(
+            cfg=MissForestConfig.model_validate(load_yaml(cfg.model_cfg_path)),
+        )
         
     def train(
         self,
-        data: Data,
         x_train: torch.tensor,
-        mask_train: torch.tensor,
+        x_true: torch.Tensor,
+        mask_train: torch.Tensor,
         experiment_writer: ExperimentWriter,
+        x_val: Optional[torch.Tensor],
+        x_true_val: Optional[torch.Tensor],
+        mask_val: Optional[torch.Tensor],
     ) -> None:
-        _ = mask_train  # MissForest operates on the full matrix; mask not required
-        _, _ = data, experiment_writer
+        _, _ = x_true, mask_train  # MissForest operates on the full matrix, thus mask is not required
+        _, _, _ = x_val, x_true_val, mask_val
+        _ = experiment_writer
         x_train = x_train.detach().cpu().numpy()
         self.missforest = MissForest(
             n_estimators=self.n_tree,
@@ -53,14 +73,12 @@ class MissForestRImputer():
         )
         self.missforest.fit(x_train)
 
-    def impute(
+    def predict(
         self,
-        data: Data,
-        x_missing: torch.tensor,
-        mask_eval: torch.tensor,
-    ):
-        _ = mask_eval  # MissForest operates on the full matrix; mask not required
-        _ = data
+        x_missing: torch.Tensor,
+        mask: Optional[torch.Tensor],
+    ) -> torch.Tensor:
+        _ = mask  # MissForest operates on the full matrix, thus mask is not required
         x_missing = x_missing.detach().cpu().numpy()
-        x_hat = self.missforest.transform(x_missing)
-        return x_hat
+        x_pred = self.missforest.transform(x_missing)
+        return torch.tensor(x_pred, device=x_missing.device)
