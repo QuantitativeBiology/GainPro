70905fc1e81ca5cb00f04b62cbedbb750b119cfb
Rita Gama <rita.gama@tecnico.ulisboa.pt>
2026-06-16 12:02:27 +0100

    chore: remove training/metrics folder creation from benchmark run
    - No longer needed since PyTorch Lightning handles loss persistence natively

--- GenerativeProteomics/utils/writers/experiment_writer.py
+++ GenerativeProteomics/utils/writers/experiment_writer.py
@@ -14,19 +14,14 @@ class ExperimentWriter:
 
         self.cfg_dir = self.run_dir / "configs"
         self.data_dir = self.run_dir / "data"
-        self.split_dir = self.run_dir / "splits"
-        self.train_dir = self.run_dir / "training"
-        self.metrics_dir = self.train_dir / "metrics"
         self.preds_dir = self.run_dir / "predictions"
         self.evaluation_dir = self.run_dir / "evaluation"
+        self.split_dir = self.run_dir / "splits"
         self.folds_dir = self.split_dir / "folds"
 
         for dir in [
             self.cfg_dir,
             self.data_dir,
-            self.split_dir,
-            self.train_dir,
-            self.metrics_dir,
             self.preds_dir,
             self.evaluation_dir,
             self.folds_dir,
@@ -34,6 +29,4 @@ class ExperimentWriter:
             dir.mkdir(parents=True, exist_ok=True)
         
         self.result_writer = ResultWriter()
-        self.metrics_writer = MetricsWriter(self.metrics_dir)
         self.metadata_writer = MetadataWriter()
-        self.split_writer = SplitWriter(self.folds_dir)
