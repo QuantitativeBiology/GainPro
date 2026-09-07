f094d755ddbce0c338672b1cf5418443d0506e15
Rita Gama <rita.gama@tecnico.ulisboa.pt>
2026-09-08 00:22:28 +0100

    fix: shift `log_transform` responsibility to user

--- GenerativeProteomics/utils/configs/dataset_config.py
+++ GenerativeProteomics/utils/configs/dataset_config.py
@@ -8,6 +8,6 @@ class DatasetConfig(BaseModel):
     name: str
     dataset_path: Path
     normalizer: Literal["minmax", "standard", "none", "auto"] = "auto"
-    log_transform: bool | Literal["auto"] = "auto"
+    log_transform: bool
     missing_mechanism: MissingMechanism = Field(default=MissingMechanism.MNAR)
     steepness: Optional[float] = Field(default=1.0, gt=0.0, le=5.0)
